cdf728e89d53cc6a8e427b93d97891f036f990fd
RaymundoZ <andrej.dron2002@gmail.com>
2023-10-03 16:56:46 +0300

    init commit

---
+++ src/main/java/com/raymundo/simplemsngr/SimpleMsngrApplication.java
@@ -0,0 +1,13 @@
+package com.raymundo.simplemsngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMsngrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMsngrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raymundo/simplemsngr/SimpleMsngrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymundo.simplemsngr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMsngrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
